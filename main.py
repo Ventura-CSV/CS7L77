b4a871563b5332f775520a5860f5492d5ac5a568
Kyu Lee <kwooler@gmail.com>
2023-01-21 22:46:17 -0800

    L77

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+##################################################
+# Comlete your code here
+##################################################
+
+# numbers = list(map(int, input().split()))
+
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
-    pass
+    numbers = input().split()
+    for i in range(len(numbers)):
+        numbers[i] = int(numbers[i])
+    print(numbers)
+
+    ######################################################
+    # Use this variable for the result
+    ######################################################
+    main.evenlist = []
+    ######################################################
+
+    # Code your program here
+
+    print(main.evenlist)
 
 
 if __name__ == '__main__':
